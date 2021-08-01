3426b7cb551b7dc9e6a8b0c4bd225cfafb68f249
Uros Jovicic <urosvgd@gmail.com>
2021-08-01 09:59:12 +0200

    Search in progress

--- lib/garage_page.dart
+++ lib/garage_page.dart
@@ -1,9 +1,12 @@
 // import 'package:moja_garaza/detail_page.dart';
+import 'package:moja_garaza/detail_page.dart';
 import 'package:moja_garaza/theme.dart';
 import 'package:flutter/material.dart';
 
 import 'models/car.dart' as car;
 import 'models/car.dart';
+
+enum SearchOptions { model, color }
 
 class GaragePage extends StatefulWidget {
   @override
@@ -11,16 +14,14 @@ class GaragePage extends StatefulWidget {
 }
 
 class _GaragePageState extends State<GaragePage> {
-  Widget appBarTitle = new Text(
-    "Search Example",
-    style: TextStyle(color: Colors.white),
-  );
-  Icon icon = new Icon(
+  SearchOptions? _options = SearchOptions.model;
+
+  Icon icon = Icon(
     Icons.search,
     color: Colors.white,
   );
   final globalKey = GlobalKey<ScaffoldState>();
-  final TextEditingController _controller = new TextEditingController();
+  final TextEditingController _controller = TextEditingController();
   late List<Car> _list;
   late bool _isSearching;
   String _searchText = "";
@@ -38,41 +39,90 @@ class _GaragePageState extends State<GaragePage> {
 
   @override
   Widget build(BuildContext context) {
+    print(searchresult);
     return Scaffold(
-      appBar: PreferredSize(
-          preferredSize: Size.fromHeight(100), child: buildAppBar(context)),
+      appBar: AppBar(
+        title: Text("Auta"),
+      ),
       backgroundColor: blackColor,
-      body: ListView(
+      body: Column(
         children: [
-          Container(
-            margin: EdgeInsets.only(left: 31, bottom: 21, top: 20),
-            child: Text(
-              'Vasa vozila',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w600,
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                controller: _controller,
+                onSubmitted: searchOperation,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.search, color: Colors.white),
+                  hintText: "Search...",
+                  hintStyle: TextStyle(color: Colors.white),
+                ),
               ),
-            ),
+              Column(
+                children: [
+                  ListTile(
+                    title: const Text('Model'),
+                    leading: Radio<SearchOptions>(
+                      value: SearchOptions.model,
+                      groupValue: _options,
+                      onChanged: (SearchOptions? value) {
+                        setState(() {
+                          _options = value;
+                        });
+                      },
+                    ),
+                  ),
+                  ListTile(
+                    title: const Text('Color'),
+                    leading: Radio<SearchOptions>(
+                      value: SearchOptions.color,
+                      groupValue: _options,
+                      onChanged: (SearchOptions? value) {
+                        setState(() {
+                          _options = value;
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              )
+            ],
           ),
-          GestureDetector(
-            onTap: () {
-              // Navigator.push(
-              //   context,
-              //   MaterialPageRoute(
-              //     builder: (context) => DetailPage(),
-              //   ),
-              // );
-            },
-            child: Expanded(
-              child: SizedBox(
-                height: MediaQuery.of(context).size.height,
-                child: ListView.builder(
-                    itemCount: _list.length,
-                    itemBuilder: (BuildContext context, index) {
-                      return buildCarsList();
-                      // return Text(_list[index].carModel);
-                    }),
-              ),
+          Expanded(
+            child: ListView(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(left: 31, bottom: 21, top: 20),
+                  child: Text(
+                    'Vasa vozila',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => DetailPage(),
+                      ),
+                    );
+                  },
+                  child: Expanded(
+                    child: SizedBox(
+                      height: MediaQuery.of(context).size.height,
+                      child: ListView.builder(
+                          itemCount: _list.length,
+                          itemBuilder: (BuildContext context, index) {
+                            return buildCarCard(_list, index);
+                          }),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
@@ -80,47 +130,60 @@ class _GaragePageState extends State<GaragePage> {
     );
   }
 
-  Widget buildCarsList() {
+  Widget buildCarCard(carList, int index) {
     return Card(
       child: Container(
-        padding: EdgeInsets.all(32.0),
-        child: Column(
-          children: <Widget>[Text('Hello World'), Text('How are you?')],
+        height: 100,
+        color: blackColor,
+        child: Row(
+          children: [
+            Center(
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: Icon(Icons.car_rental_rounded),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                alignment: Alignment.topLeft,
+                child: Column(
+                  children: [
+                    Expanded(
+                      flex: 5,
+                      child: ListTile(
+                        isThreeLine: true,
+                        title:
+                            Text("Proizvodnja: ${carList[index].carProducer}"),
+                        subtitle: Text("Model ${carList[index].carModel}"),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 5,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Text("Boja: ${carList[index].color}"),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          Text("Reg.Br.: ${carList[index].plateNumber}"),
+                          SizedBox(
+                            height: 100,
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              flex: 8,
+            ),
+          ],
         ),
       ),
+      elevation: 8,
+      margin: EdgeInsets.all(10),
     );
-  }
-
-  Widget buildAppBar(BuildContext context) {
-    return new AppBar(centerTitle: true, title: appBarTitle, actions: <Widget>[
-      new IconButton(
-        icon: icon,
-        onPressed: () {
-          setState(() {
-            if (this.icon.icon == Icons.search) {
-              this.icon = new Icon(
-                Icons.close,
-                color: Colors.white,
-              );
-              this.appBarTitle = new TextField(
-                controller: _controller,
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-                decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.search, color: Colors.white),
-                    hintText: "Search...",
-                    hintStyle: TextStyle(color: Colors.white)),
-                onChanged: searchOperation,
-              );
-              _handleSearchStart();
-            } else {
-              _handleSearchEnd();
-            }
-          });
-        },
-      ),
-    ]);
   }
 
   void _handleSearchStart() {
@@ -131,13 +194,9 @@ class _GaragePageState extends State<GaragePage> {
 
   void _handleSearchEnd() {
     setState(() {
-      this.icon = new Icon(
+      this.icon = Icon(
         Icons.search,
         color: Colors.white,
-      );
-      this.appBarTitle = new Text(
-        "Search Sample",
-        style: new TextStyle(color: Colors.white),
       );
       _isSearching = false;
       _controller.clear();
@@ -147,9 +206,20 @@ class _GaragePageState extends State<GaragePage> {
   void searchOperation(String searchText) {
     searchresult.clear();
     for (int i = 0; i < _list.length; i++) {
-      String data = _list[i].carModel;
-      if (data.toLowerCase().contains(searchText.toLowerCase())) {
-        searchresult.add(data);
+      if (_options == SearchOptions.model) {
+        String data = _list[i].carModel;
+
+        if (data.toLowerCase().contains(searchText.toLowerCase())) {
+          searchresult.add(data);
+        }
+      }
+
+      if (_options == SearchOptions.color) {
+        String data = _list[i].color;
+
+        if (data.toLowerCase().contains(searchText.toLowerCase())) {
+          searchresult.add(data);
+        }
       }
     }
   }
